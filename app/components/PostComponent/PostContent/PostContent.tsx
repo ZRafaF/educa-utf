e2735ce283e9d75bd194d551ff20ac8f4b9f062d
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-09-15 17:21:40 -0300

    not working because of use client

--- app/components/PostComponent/PostContent/PostContent.tsx
+++ app/components/PostComponent/PostContent/PostContent.tsx
@@ -8,10 +8,49 @@ import Markdown from 'markdown-to-jsx';
 import CustomButton from './CustomButton';
 import './github-markdown.css';
 import Box from '@mui/material/Box/Box';
+//@ts-ignore
+import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+//@ts-ignore
+import { Prism } from 'react-syntax-highlighter';
 
 interface PostContentProps {
 	article: string;
 }
+
+const CodeBlock = ({
+	className,
+	children,
+}: {
+	className: string;
+	children: string;
+}) => {
+	let lang = 'text'; // default monospaced text
+	if (className && className.startsWith('lang-')) {
+		lang = className.replace('lang-', '');
+	}
+	return (
+		<Prism language={lang} style={darcula}>
+			{children}
+		</Prism>
+	);
+};
+
+// markdown-to-jsx uses <pre><code/></pre> for code blocks.
+const PreBlock = ({ children, ...rest }: { rest: any; children: any }) => {
+	if ('type' in children && children['type'] === 'code') {
+		return CodeBlock(children['props']);
+	}
+	return (
+		<pre
+			{...rest}
+			style={{
+				borderRadius: '10px !important',
+			}}
+		>
+			{children}
+		</pre>
+	);
+};
 
 const PostContent: FunctionComponent<PostContentProps> = ({ article }) => {
 	return (
@@ -20,6 +59,7 @@ const PostContent: FunctionComponent<PostContentProps> = ({ article }) => {
 				options={{
 					overrides: {
 						CustomButton,
+						pre: PreBlock,
 					},
 				}}
 			>
