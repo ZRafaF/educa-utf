fc5dac16a0837b32742e6764b4a85cef7a78f7cc
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2023-09-16 15:37:37 -0300

    auto close drawer on navigation, changed highlight color

--- app/components/AppOverlay/OverlayController/DrawerControllers.tsx
+++ app/components/AppOverlay/OverlayController/DrawerControllers.tsx
@@ -4,12 +4,13 @@
 // https://opensource.org/licenses/MIT
 'use client';
 
-import { FunctionComponent, ReactNode, useContext } from 'react';
+import { FunctionComponent, ReactNode, useContext, useEffect } from 'react';
 import drawerWidth from '../../../lib/drawerWidth';
 import { styled } from '@mui/material/styles';
 import MuiDrawer from '@mui/material/Drawer';
 import { OverlayControllerContext } from '@/contexts/OverlayControllerProvider';
 import Backdrop from '@mui/material/Backdrop';
+import { usePathname } from 'next/dist/client/components/navigation';
 
 interface ControllerProps {
 	children: ReactNode;
@@ -43,6 +44,11 @@ const Drawer = styled(MuiDrawer, {
 
 const DrawerController: FunctionComponent<ControllerProps> = ({ children }) => {
 	const [open, setOpen] = useContext(OverlayControllerContext);
+	const pathname = usePathname();
+
+	useEffect(() => {
+		setOpen(false);
+	}, [pathname]);
 
 	return (
 		<Drawer variant="permanent" open={open}>
