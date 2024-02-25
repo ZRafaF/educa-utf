5b751a62ea3ec9ba9c63000d1e9860d869ca2daf
Caracol <rafaelfariasm@live.com>
2024-02-25 18:38:40 -0300

    fixed tag overlay

--- app/(app)/loading.tsx
+++ app/(app)/loading.tsx
@@ -1,30 +1,17 @@
-import BestChapters from './BestChapters';
+// Copyright (c) 2023 Rafael Farias
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 import { Suspense } from 'react';
 import Box from '@mui/material/Box/Box';
 import BannerSomethingNew from '@/components/BannerSomethingNew/BannerSomethingNew';
-import BestArticles from './BestArticles';
 import Divider from '@mui/material/Divider/Divider';
-import { Metadata } from 'next/types';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2/Grid2'; // Grid version 2
 import DummyCard from '@/components/DummyCard/DummyCard';
 
-export const revalidate = 60;
-
-export const metadata: Metadata = {
-	title: 'EducaUTF',
-	description:
-		'Web aplicação para criação e compartilhamento de conteúdo acadêmico. Feito por alunos para o mundo :)',
-	keywords: ['EducaUTF', 'Educa UTF', 'artigos', 'estudo', 'UTFPR', 'wiki'],
-	openGraph: {
-		images: '/icon-256x256.png',
-	},
-	twitter: {
-		images: '/icon-256x256.png',
-	},
-};
-
-export default function Home() {
+export default function Loading() {
 	const dummyArrayChapters: number[] = Array(10).fill(1);
 	const dummyArrayArticles: number[] = Array(12).fill(1);
 
@@ -51,22 +38,18 @@ export default function Home() {
 							},
 						}}
 					>
-						<Suspense
-							fallback={dummyArrayChapters.map((_, idx) => (
-								<Grid
-									key={`dummy_chapter_${idx}`}
-									xs={6}
-									sm={4}
-									md={4}
-									lg={3}
-									xl={2.4}
-								>
-									<DummyCard type="chapter" />
-								</Grid>
-							))}
-						>
-							<BestChapters />
-						</Suspense>
+						{dummyArrayChapters.map((_, idx) => (
+							<Grid
+								key={`dummy_chapter_${idx}`}
+								xs={6}
+								sm={4}
+								md={4}
+								lg={3}
+								xl={2.4}
+							>
+								<DummyCard type="chapter" />
+							</Grid>
+						))}
 					</Grid>
 				</Box>
 				<Divider
@@ -91,22 +74,18 @@ export default function Home() {
 							},
 						}}
 					>
-						<Suspense
-							fallback={dummyArrayArticles.map((_, idx) => (
-								<Grid
-									key={`dummy_articles_${idx}`}
-									xs={15}
-									sm={6}
-									md={6}
-									lg={4}
-									xl={4}
-								>
-									<DummyCard type="article" />
-								</Grid>
-							))}
-						>
-							<BestArticles />
-						</Suspense>
+						{dummyArrayArticles.map((_, idx) => (
+							<Grid
+								key={`dummy_articles_${idx}`}
+								xs={15}
+								sm={6}
+								md={6}
+								lg={4}
+								xl={4}
+							>
+								<DummyCard type="article" />
+							</Grid>
+						))}
 					</Grid>
 				</Box>
 			</Box>
