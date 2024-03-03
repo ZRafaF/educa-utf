b285c6761c82e55f807360993a75091796bf6055
Caracol <rafaelfariasm@live.com>
2024-03-03 18:27:22 -0300

    started implementing the radial exercise

--- app/plugins/RadialSelector/RadialSelector.tsx
+++ app/plugins/RadialSelector/RadialSelector.tsx
@@ -1,8 +1,13 @@
-import { FunctionComponent } from 'react';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import { FunctionComponent, useMemo } from 'react';
 
 interface RadialSelectorProps {
 	multiple?: boolean;
-	options: string[];
+	options: string;
 	answare: string;
 }
 
@@ -11,7 +16,41 @@ const RadialSelector: FunctionComponent<RadialSelectorProps> = ({
 	options,
 	answare,
 }) => {
-	return <>{options}</>;
+	const optionsArray = useMemo(() => options.split(','), [options]);
+
+	const optionsObj = useMemo(() => {
+		console.log(options);
+
+		try {
+			return JSON.parse(options);
+		} catch (error) {
+			return {};
+		}
+	}, [options]);
+
+	console.log(optionsObj);
+
+	return (
+		<Box>
+			options: {options}
+			<FormControl>
+				<RadioGroup
+					aria-labelledby="demo-radio-buttons-group-label"
+					defaultValue="female"
+					name="radio-buttons-group"
+				>
+					{optionsArray.map((option, index) => (
+						<FormControlLabel
+							key={index}
+							value={option}
+							control={<Radio />}
+							label={option}
+						/>
+					))}
+				</RadioGroup>
+			</FormControl>
+		</Box>
+	);
 };
 
 export default RadialSelector;
--- app/plugins/RadialSelector/RadialSelectorEditor.tsx
+++ app/plugins/RadialSelector/RadialSelectorEditor.tsx
@@ -1,24 +1,29 @@
 'use client';
 
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useMemo } from 'react';
 import { PluginEditorProps } from '../PluginsTypes';
 import Button from '@mui/material/Button';
 
 const RadialSelectorEditor: FunctionComponent<PluginEditorProps> = ({
 	returnFunction,
 }) => {
+	const optionsString = useMemo(() => {
+		return JSON.stringify({
+			text: 'asd',
+			answer: 'asd',
+		});
+	}, []);
+
 	return (
-		<>
-			<Button
-				onClick={() => {
-					returnFunction(
-						`<RadialSelector options="${['asd', 'asd']}"/>`
-					);
-				}}
-			>
-				click
-			</Button>
-		</>
+		<Button
+			onClick={() => {
+				returnFunction(
+					`<RadialSelector options='${optionsString}'/>\n`
+				);
+			}}
+		>
+			click
+		</Button>
 	);
 };
 
